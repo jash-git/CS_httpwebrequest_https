fb7d4ebe9ad0c7a4d00269bdb5956e06778de5f7
jash-git <jash.liao@gmail.com>
2018-04-19 15:42:12 +0800

    實作三個網站的程式碼

--- cs_httpwebrequest_https/Program.cs
+++ cs_httpwebrequest_https/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Net;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 namespace cs_httpwebrequest_https
 {
@@ -14,19 +16,94 @@ namespace cs_httpwebrequest_https
             Console.Write("Press any key to continue . . . ");
             Console.ReadKey(true);
         }
+
+        static private bool CheckValidationResult(object sender,X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        {
+            return true;// Always accept
+        }
+        static private HttpWebResponse getUrlResponse(string url)
+        {
+            HttpWebResponse resp = null;
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+
+            if (url.StartsWith("https", StringComparison.OrdinalIgnoreCase))
+            {
+                ServicePointManager.ServerCertificateValidationCallback =
+                        new RemoteCertificateValidationCallback(CheckValidationResult);
+            }
+
+            //...
+            resp = (HttpWebResponse)req.GetResponse();
+            using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+            {
+                String results = sr.ReadToEnd();
+                Console.WriteLine(results);
+                sr.Close();
+            }
+            
+            //...
+            return resp;
+        }
+        private static string PostUrl(string url, string postData)
+        {
+            HttpWebRequest request = null;
+            if (url.StartsWith("https", StringComparison.OrdinalIgnoreCase))
+            {
+                request = WebRequest.Create(url) as HttpWebRequest;
+                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+                request.ProtocolVersion = HttpVersion.Version11;
+　　　　　　　　　// 這裡設置了協議類型。
+
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;// SecurityProtocolType.Tls1.2; 
+                //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+
+                request.KeepAlive = false;
+                ServicePointManager.CheckCertificateRevocationList = true;
+                ServicePointManager.DefaultConnectionLimit = 100;
+                ServicePointManager.Expect100Continue = false;
+            }
+            else
+            {
+                request = (HttpWebRequest)WebRequest.Create(url);
+            }
+
+            request.Method = "POST";//使用get方式發送數據
+            request.ContentType = null;
+            request.Referer = null;
+            request.AllowAutoRedirect = true;
+            request.UserAgent = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727)";
+            request.Accept = "*/*";
+
+            byte[] data = Encoding.UTF8.GetBytes(postData);
+            Stream newStream = request.GetRequestStream();
+            newStream.Write(data, 0, data.Length);
+            newStream.Close();
+
+			//獲取網頁響應結果
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+			//client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+            string result = string.Empty;
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                result = sr.ReadToEnd();
+            }
+
+            return result;
+        }
         static void Main(string[] args)
         {
-            /*
-                資料來源:https://shunnien.github.io/2017/07/13/Accessing-HTTPS-URL-using-csharp/
-            */ 
+            //*
+            //語法01
+            //資料來源:https://shunnien.github.io/2017/07/13/Accessing-HTTPS-URL-using-csharp/
             var url = "https://www.moi.gov.tw/";//台灣內政部網址
             string results;
             // 強制認為憑證都是通過的，特殊情況再使用
-            //ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.AutomaticDecompression = DecompressionMethods.GZip;
             // 加入憑證驗證
-            request.ClientCertificates.Add(new System.Security.Cryptography.X509Certificates.X509Certificate());
+            //request.ClientCertificates.Add(new System.Security.Cryptography.X509Certificates.X509Certificate());
             HttpWebResponse resp = (HttpWebResponse)request.GetResponse();
             using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
             {
@@ -34,6 +111,21 @@ namespace cs_httpwebrequest_https
                 sr.Close();
             }
             Console.WriteLine(results);
+            pause();
+            //*/
+
+            //*
+            //語法02
+            getUrlResponse("https://www.moi.gov.tw/");
+            pause();
+            //*/
+
+            //*
+            //語法3
+            string url1 = "https://www.moi.gov.tw/";
+            string result = PostUrl(url1, "key=123");
+            Console.WriteLine(result);
+            //*/ 
             pause();
         }
     }
